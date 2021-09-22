8cee5d83607273d1a80fdcd034153a311ff1f868
Sheldon Hull <sheldonhull@users.noreply.github.com>
2021-09-22 16:30:18 -0500

    feat(gittools): ✨ add gittools to install common go git tooling like git-town and bit for devcontainer onboarding

---
+++ gittools/gittools.go
@@ -0,0 +1,28 @@
+// Package gittools provides automatic setup of some useful git tooling like Bit and Git Town
+package gittools
+
+import (
+	"github.com/magefile/mage/mg"
+	"github.com/pterm/pterm"
+	"github.com/sheldonhull/magetools/tooling"
+)
+
+type Gittools mg.Namespace
+
+// golang tools to ensure are locally vendored.
+var toolList = []string{ //nolint:gochecknoglobals // ok to be global for tooling setup
+	"github.com/git-town/git-town@latest",
+	"github.com/chriswalz/bit@latest",
+}
+
+// ⚙️  Init runs all required steps to use this package.
+func (Gittools) Init() error {
+	if err := tooling.InstallTools(toolList); err != nil {
+		return err
+	}
+	pterm.Info.Println("🔧  Installed tools. To setup aliases automatically run these statements in your terminal:")
+	pterm.Info.Println("bit aliases:     \tbit complete")
+	pterm.Info.Println("git-town aliases:\tgit town alias true")
+
+	return nil
+}
---
+++ gittools/gittools_test.go
@@ -0,0 +1,18 @@
+package gittools_test
+
+import (
+	"testing"
+
+	iz "github.com/matryer/is"
+	"github.com/pterm/pterm"
+	"github.com/sheldonhull/magetools/gittools"
+)
+
+func TestGolang_InitAndClean(t *testing.T) {
+	is := iz.New(t)
+	pterm.DisableColor()
+	pterm.DisableStyling()
+
+	err := gittools.Gittools{}.Init()
+	is.NoErr(err) // Init should not fail
+}
