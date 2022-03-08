ace09f9a052a898a85ab942d57f1f98761595950
Sheldon Hull <sheldonhull@users.noreply.github.com>
2022-03-07 19:50:31 -0600

    docs(examples): ➕ RENAME examples to .examples (code coverage fix) & add examples of asdf installer commands using mage

---
+++ .examples/asdf/main.go
@@ -0,0 +1,134 @@
+//go:build examples
+
+// Package asdf helps configure your current workspace by reading the asdf tools file and installing required plugins and the associated apps.
+package asdf
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"time"
+
+	script "github.com/bitfield/script"
+	humanize "github.com/dustin/go-humanize"
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+	"github.com/pterm/pterm"
+	mtu "github.com/sheldonhull/magetools/pkg/magetoolsutils"
+)
+
+func checklinux() {
+	if runtime.GOOS != "Linux" {
+		_ = mg.Fatalf(1, "this command is only supported on Linux and you are on: %s", runtime.GOOS)
+	}
+}
+
+// Install contains tasks normally relegated to bash scripts, mostly focused on linux/macOS compatibility.
+type Install mg.Namespace
+
+// relTime returns just a simple relative time humanized, without the "ago" suffix.
+func relTime(t time.Time) string {
+	return strings.ReplaceAll(humanize.Time(t), " ago", "")
+}
+
+// ⚙️ Asdf installs tooling.
+//
+// NOTE: 👀 Will try to modify with native Git package commands later, just a quick draft.
+func (Install) Asdf() error {
+	checklinux()
+	mtu.CheckPtermDebug()
+	start := time.Now()
+	defer func(start time.Time) {
+		pterm.Success.Printf("✅ (Install) Asdf() [%s]\n", relTime(start))
+	}(start)
+	pterm.DefaultHeader.Println("⚙️ Direnv")
+
+	dirname, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	p := script.Exec("bash -c 'git clone https://github.com/asdf-vm/asdf.git " + dirname + "/.asdf --branch v0.9.0'")
+	output, err := p.String()
+
+	if p.ExitStatus() == 128 { //nolint:gomnd // gomnd: exit code is specific to git
+		pterm.Warning.Println("likely already cloned this repo since exit code 128")
+	} else {
+		pterm.Error.PrintOnError(err)
+	}
+	pterm.Info.Println(output)
+
+	script.Exec("bash -c 'source " + dirname + "/.asdf/.asdf/asdf.sh'")
+	pterm.Warning.Printfln(
+		"You need to add this to your .zshrc.\n\nsource $HOME/.asdf/asdf.sh\n\n",
+	)
+	return nil
+}
+
+// 💾 Apps will install Asdf plugins and the apps based on .tool-versions. Failures on install are warning level only, and failure only occurs when asdf not found at all.
+func (Install) Apps() error { //nolint:funlen // It's long and done for bootstrapping asdf apps, ok to leave as the ugly func it is.
+	mtu.CheckPtermDebug()
+
+	asdfBinary, err := exec.LookPath("asdf")
+	if err != nil {
+		pterm.Error.Printfln(
+			"asdf binary not found. have you sourced this? " + "You need to add this to your .zshrc.\n\nsource $HOME/.asdf/asdf.sh\n\n",
+		)
+	}
+
+	f, err := os.Open(".tool-versions")
+	if err != nil {
+		return err
+	}
+	// Remember to close the file at the end of the program.
+	defer f.Close()
+
+	// Read the file line by line using scanner.
+	scanner := bufio.NewScanner(f)
+	var pluginName, pluginVersion string
+	var issuecount int
+
+	if err := sh.RunV(asdfBinary, "plugin", "update", "--all"); err != nil {
+		pterm.Warning.Printfln("[asdf] plugin update failure: %v", err)
+	}
+
+	for scanner.Scan() {
+		pterm.Debug.Printfln("[asdf] %q", scanner.Text())
+		line := strings.Split(scanner.Text(), " ")
+		if len(line) != 2 { //nolint:gomnd // gomnd: array should equal this exactly.
+			pterm.Warning.Printfln("[asdf] unstable to split: %v", err)
+			continue
+		}
+
+		pluginName = line[0]
+		pluginVersion = line[1]
+		p := script.Exec(fmt.Sprintf("bash -c '%s plugin add %s'", asdfBinary, pluginName))
+		out, err := p.String()
+
+		if p.ExitStatus() == 2 { //nolint:gomnd // gomnd: valid exit code for already installed.
+			pterm.Success.Printfln("[asdf] ✅ %q is already added", pluginName)
+			continue
+		} else if p.ExitStatus() != 0 {
+			issuecount++
+			pterm.Warning.Printfln("[asdf] failure to add plugin: %v: %q err: %v", pluginName, out, err)
+			continue
+		}
+
+		if err := sh.RunV(asdfBinary, "install", pluginName, pluginVersion); err != nil {
+			issuecount++
+			pterm.Warning.Printfln("[asdf] install failure: %v", err)
+			continue
+		}
+		pterm.Success.Printfln("[asdf] ✅ %q added [version for install: %s]", pluginName, pluginVersion)
+	}
+
+	if issuecount > 0 {
+		pterm.Warning.Printfln("[asdf] total install issues: %d", issuecount)
+		return nil
+	}
+	pterm.Success.Println("[asdf] plugin install successful")
+	return nil
+}
