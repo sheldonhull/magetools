ad0767d1eb9818c186a612bb3f68840c86876fe8
Sheldon Hull <sheldonhull@users.noreply.github.com>
2024-05-21 12:36:42 -0500

    refactor: support windows with trunk package

--- trunk/trunk_unix.go
+++ trunk/trunk_unix.go
@@ -1,11 +1,11 @@
+// go:build linux darwin
+
 // Package trunk contains tasks to automate setup and usage in Mage based projects for the great trunk.io tooling.
 package trunk
 
 import (
-	"errors"
 	"os"
 	"os/exec"
-	"runtime"
 
 	"github.com/bitfield/script"
 	"github.com/magefile/mage/mg"
@@ -25,23 +25,10 @@ func (Trunk) Init() {
 	)
 }
 
-// checkCompatibility checks the current OS and warns if trunk.io is not supported with an error that can be either bypassed or handled.
-func checkCompatibility() error {
-	magetoolsutils.CheckPtermDebug()
-	if runtime.GOOS == "windows" {
-		pterm.Warning.Printfln("trunk.io is not supported on: %s", runtime.GOOS)
-		return errors.New("trunk.io is not supported on: " + runtime.GOOS)
-	}
-	return nil
-}
-
 // ⚙️ InstallTrunk installs trunk.io tooling if it isn't already found.
 func (Trunk) Install() error {
 	magetoolsutils.CheckPtermDebug()
-	if err := checkCompatibility(); err != nil {
-		pterm.Warning.Printfln("[non-terminating] skipping trunk.io installation due to incompatible OS: %v", err)
-		return nil
-	}
+
 	_, err := exec.LookPath("trunk")
 	if err != nil && os.IsNotExist(err) {
 		pterm.Warning.Printfln("unable to resolve aqua cli tool, please install for automated project tooling setup: https://aquaproj.github.io/docs/tutorial-basics/quick-start#install-aqua")
@@ -57,18 +44,10 @@ func (Trunk) Install() error {
 
 // ⚙️ InstallPlugins ensures the required runtimes are installed.
 func (Trunk) InstallPlugins() error {
-	if err := checkCompatibility(); err != nil {
-		pterm.Warning.Printfln("[non-terminating] skipping trunk.io installation due to incompatible OS: %v", err)
-		return nil
-	}
 	return sh.RunV("trunk", "install")
 }
 
 // ⚙️ Upgrade upgrades trunk using itself and also the plugins.
 func (Trunk) Upgrade() error {
-	if err := checkCompatibility(); err != nil {
-		pterm.Warning.Printfln("[non-terminating] skipping trunk.io installation due to incompatible OS: %v", err)
-		return nil
-	}
-	return sh.RunV("trunk", "install")
+	return sh.RunV("trunk", "upgrade")
 }
